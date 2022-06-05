582605c9a55f911e3f881ea16532e776b771bdc7
Jeremias Marinho <jeremias10@msn.com>
2022-06-05 06:50:51 -0300

    Conclusão da estilização

---
+++ src/components/Comment.jsx
@@ -0,0 +1,29 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from './Comment.module.css'
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/jeremiasmarinho.png" />
+        <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Jeremias Marinho</strong>
+              <time title="3 de junho às 08:13h" dateTime="2022-06-03 08:13:30">Cerca de uma hora atrás</time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+           <button>
+             <ThumbsUp />
+             Aplaudir <span>20</span>
+           </button>
+        </footer>
+        </div>
+      </div>
+  )
+}
