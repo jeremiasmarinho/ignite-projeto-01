7d45aa101c88ab63485ac556fdb34e4fe66a58ab
Jeremias Marinho <jeremias10@msn.com>
2022-06-05 07:50:47 -0300

    Conclusão das paginas

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,10 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from './Comment.module.css'
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/jeremiasmarinho.png" />
+      <Avatar hasBorder={false} src="https://github.com/jeremiasmarinho.png" />
         <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
 import capa from '../assets/capa.svg' 
+import { Avatar } from './Avatar';
 export function Sidebar(){
   return (
     <aside className={styles.sidebar}>
       <img className={styles.cover} src={capa} />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/87774323?v=4" alt="Foto de perfil" />
+        <Avatar hasBorder={true} src="https://avatars.githubusercontent.com/u/87774323?v=4"/>
 
         <strong>Jeremias Marinho</strong>
         <span>Web Develop</span>
