e8018fb778d9fa30da43297b005957257aa7446a
Jeremias Marinho <jeremias10@msn.com>
2022-06-05 08:47:14 -0300

    Conclusão do formato das horas

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,36 @@ import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import "./global.css"
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/jeremiasmarinho.png',
+      name: 'Jeremias Marinho',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+     ],
+     publishedAt: new Date('2022-06-05 08:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/jaelsonrc.png',
+      name: 'Jaelson Cunha',
+      role: 'Web Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+     ],
+     publishedAt: new Date('2022-06-01 09:00:00'),
+  }
+];
 
 export function App() {
   
@@ -16,14 +46,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post 
-          author="Jeremias Marinho" 
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quidem atque ipsa eaque, commodi, nostrum recusandae voluptates veniam cumque doloribus beatae asperiores molestias placeat odit expedita tempore praesentium veritatis itaque."
-        />
-         <Post 
-          author="Jeremias Marinho" 
-          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam quidem atque ipsa eaque, commodi, nostrum recusandae voluptates veniam cumque doloribus beatae asperiores molestias placeat odit expedita tempore praesentium veritatis itaque."
-        />
+          {posts.map(post => {
+            return (
+            <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+            />
+            )
+          })}
      
      
         </main>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,45 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, publishedAt, content}) {
+  const publishedDateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", { 
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  })
+
   return (
     <article className={styles.post}>
         <header>
           <div className={styles.author}>
-            <Avatar hasBorder={true} src="https://github.com/jeremiasmarinho.png" />
+            <Avatar hasBorder={true} src={author.avatarUrl} />
             <div className={styles.authorInfo}>
-              <strong>Jeremias Marinho</strong>
-              <span>Web Developer</span>
+              <strong>{author.name}</strong>
+              <span>{author.role}</span>
             </div>
           </div>
 
-          <time title="3 de junho às 08:13h" dateTime="2022-06-03 08:13:30">Publicado há 1 hora</time>
+          <time 
+            title={publishedDateFormatted} 
+            dateTime={publishedAt.toISOString()}>
+             {publishedDateRelativeToNow}
+          </time>
         </header>
           <div className={styles.content}>
-
-           <p>Fala galeraa 👋</p>
-
-           <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-           <p>👉{' '}<a href="#"> jane.design/doctorcare</a></p>
-
-           <p>
-              <a href="#">#novoprojeto </a>
-              <a href="#">#nlw </a>
-              <a href="#">#rocketseat</a>
-            </p>
+            {content.map(line => {
+              if (line.type === 'paragraph') {
+                return <p>{line.content}</p>
+              } else if (line.type === 'link') {
+                return <p><a href="#">{line.content}</a></p>
+              }
+            })}
 
           </div>
 
